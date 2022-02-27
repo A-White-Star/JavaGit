a141108d77cfe2cebc35402111ea8566eea3b865
Whitecat <byx102930@qq.com>
2022-02-27 17:10:23 +0800

    创建exam管理系统，完成初步的数据库创建以及对应的class创建

---
+++ exam_System/ExamAdmin/src/test/java/com/white/examadmin/ExamAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.white.examadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
